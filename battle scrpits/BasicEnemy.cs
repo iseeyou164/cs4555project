7c623d90b7e7316d9a9f67a74666f51ea9b30724
egarci254 <124205257+egarci254@users.noreply.github.com>
2025-11-04 15:46:10 -0800

    Delete battle scrpits directory

--- battle scrpits/BasicEnemy.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class BasicEnemy : MonoBehaviour
-{
-    public int maxHealth = 100;
-    public int attackPower = 20;
-}
--- battle scrpits/BattleData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class BattleData
-{
-    public static GameObject enemyPrefab; // You can use a prefab or data object
-    public static GameObject playerPrefab;
-}
-
-
