d337d76b055f92cdd34a1a925b897b7194cddc2e
egarci254 <ervingarcia1710@icloud.com>
2025-11-17 00:22:08 -0800

    idle animations
    idle animations

---
+++ Assets/All battle related/Scripts 4 battle/GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public static class GameState
+{
+    public static Dictionary<string, int> playerTileIndices = new Dictionary<string, int>();
+
+    public static int currentPlayerIndex = 0;
+
+    public static bool returningFromBattle = false;
+}
